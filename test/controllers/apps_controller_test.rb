9e084c79ca17525ce3039d54385197b4cbe0ca5d
Gerson <geric99997@gmail.com>
2020-08-10 12:44:44 -0400

    Add testing to /apps endpoint

--- test/controllers/apps_controller_test.rb
+++ test/controllers/apps_controller_test.rb
@@ -4,4 +4,8 @@ class AppsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "api endpoint exist and work" do
+    assert_routing({method: 'get', path: '/apps'}, {controller: "apps", action: "index"}) 
+  end
 end
